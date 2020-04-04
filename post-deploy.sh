c7837d12deaf8bececcf534b40622c2c9da0c3c7
JoshMLeslie <hello@joshmleslie.com>
2020-04-04 17:47:23 -0400

    Add CNAME

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,6 +1,9 @@
 echo 'please wait, pushing CNAME to gh-pages'
 # get current branch name
 currentBranch=$( git rev-parse --abbrev-ref HEAD )
+currentTime=$(date +"%D_%I:%M_%p")
+# stash any changes
+git stash -ul -m $currentTime
 # switch to github pages branch
 git checkout gh-pages
 # create CNAME file and push
@@ -10,5 +13,7 @@ git commit -m 'Add CNAME'
 git push
 # switch back to original branch
 git checkout $currentBranch
+# reapply any changes
+git stash pop
 
 echo 'CNAME file pushed, operation complete.'
